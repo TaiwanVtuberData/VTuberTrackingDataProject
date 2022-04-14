cfe7888c5cd5cb0099bfbf460e442d646672857d
nh60211as <nh60211@gmail.com>
2022-04-14 11:47:43 +0800

    Add GenerateGraph project

---
+++ GenerateGraph/Program.cs
@@ -0,0 +1,196 @@
+﻿using Common.Types;
+using Common.Utils;
+using GenerateGraph;
+using Microsoft.VisualBasic.FileIO;
+
+int recentDays = -1;
+if (args.Length == 1)
+    recentDays = int.Parse(args[0]);
+
+string csvDirectory = GetCsvDirectory("./CsvDirectory");
+TrackList trackList = new(csvFilePath: "./TW_VTUBER_TRACK_LIST.csv", requiredLevel: 999, throwOnValidationFail: true);
+
+WriteDateTimeStatistics(trackList, csvDirectory, recentDays, byGroup: false, "Individual");
+WriteDateTimeStatistics(trackList, csvDirectory, recentDays, byGroup: true, "Group");
+
+static string GetCsvDirectory(string filePath)
+{
+    string? line;
+
+    // Read the file and display it line by line.  
+    using StreamReader file = new(filePath);
+    if ((line = file.ReadLine()) != null)
+    {
+        file.Close();
+        return line;
+    }
+
+    throw new Exception("Could not retrieve Csv directory key.");
+}
+
+// ignore recentDays if it's lower than 0
+static void WriteDateTimeStatistics(TrackList trackList, string recordDirectory, int recentDays, bool byGroup, string writePrefix)
+{
+    List<Tuple<FileInfo, DateTime>> csvFileList = FileUtility.GetFileInfoDateTimeList(recordDirectory, recentDays);
+
+    StatisticsTable statisticsTable = new(trackListFilePath: "./TW_VTUBER_TRACK_LIST.csv", requiredLevel: 999, byGroup);
+    foreach (Tuple<FileInfo, DateTime> fileInfoDateTime in csvFileList)
+    {
+        Dictionary<string, VTuberStatistics> statisticsDictionary = GetStatisticsDictionaryFromRecordCSV(trackList, fileInfoDateTime.Item1.FullName, byGroup);
+
+        bool shouldAdd = false;
+        if (byGroup)
+        {
+            if (statisticsDictionary.Count >= trackList.GetGroupNameList().Count * 0.8)
+            {
+                shouldAdd = true;
+            }
+        }
+        else
+        {
+            if (statisticsDictionary.Count >= trackList.GetCount() * 0.5)
+            {
+                shouldAdd = true;
+            }
+        }
+
+        if (shouldAdd)
+        {
+            statisticsTable.AddRow(dateTime: fileInfoDateTime.Item2, statisticsDictionary);
+        }
+    }
+    statisticsTable.FillEmptyValueByInterpolation();
+
+    string[] names = {
+                "YouTube.SubscriberCount",
+                "YouTube.ViewCount",
+                "YouTube.RecentMedianViewCount",
+                "YouTube.RecentHighestViewCount",
+                //"YouTube.SubscriberCountToMedianViewCount",
+                "Twitch.FollowerCount",
+                "Twitch.RecentMedianViewCount",
+                "Twitch.RecentHighestViewCount",
+                "Twitch.FollowerCountToMedianViewCount",
+                "CombinedRecentMedianViewCount",
+            };
+
+    foreach (string name in names)
+    {
+        WriteIndividualRecordCSV(statisticsTable, name, null, writePrefix);
+    }
+
+    string[] namesConstriant = {
+                "YouTube.SubscriberCountToMedianViewCount",
+            };
+
+    foreach (string name in namesConstriant)
+    {
+        WriteIndividualRecordCSV(statisticsTable, name, 2000m, writePrefix);
+    }
+}
+
+static Dictionary<string, VTuberStatistics> GetStatisticsDictionaryFromRecordCSV(TrackList trackList, string filePath, bool byGroup)
+{
+    // CSV Format:
+    // Name,SubscriberCount,ViewCount,MedianViewCount,HighestViewCount
+    // 鳥羽樂奈,40600,1613960,9725,23248
+    // 香草奈若,26900,1509583,15267,57825
+
+    TextFieldParser reader = new(filePath)
+    {
+        HasFieldsEnclosedInQuotes = true,
+        Delimiters = new string[] { "," },
+        CommentTokens = new string[] { "#" },
+        TrimWhiteSpace = false,
+        TextFieldType = FieldType.Delimited,
+    };
+
+    // consume header
+    string[]? headerBlock = reader.ReadFields();
+
+    if (headerBlock is null)
+        return new();
+
+    VTuberStatistics.Version version = VTuberStatistics.GetVersionByHeaderLength(headerBlock.Length);
+    if (version == VTuberStatistics.Version.Unknown)
+        return new();
+
+    Dictionary<string, VTuberStatistics> ans = new();
+    int entryCount = 0;
+    int groupEntryCount = 0;
+    while (!reader.EndOfData)
+    {
+        string[]? entryBlock = reader.ReadFields();
+        if (entryBlock is null)
+            continue;
+
+        entryCount++;
+
+        string name = entryBlock[0];
+        string displayName = trackList.GetDisplayName(name);
+
+        if (byGroup == false)
+        {
+            ans.Add(displayName, new VTuberStatistics(entryBlock));
+        }
+        else
+        {
+            string groupName = trackList.GetGroupNameByName(displayName);
+            if (groupName == "")
+                continue;
+
+            groupEntryCount++;
+
+            if (!ans.ContainsKey(groupName))
+                ans.Add(groupName, new VTuberStatistics(entryBlock));
+            else
+                ans[groupName].Add(entryBlock);
+        }
+    }
+
+    if (byGroup)
+    {
+        if (groupEntryCount <= trackList.GetVtuberWithGroupCount() * 0.7)
+            return new();
+    }
+
+    return ans;
+}
+
+static void WriteIndividualRecordCSV(StatisticsTable statisticsTable, string writeColumnName, decimal? subConstriant, string writePrefix)
+{
+    List<DateTime> dateList = statisticsTable.GetDateTimeList();
+    dateList.Sort();
+    DateTime minDate = dateList.Min();
+    DateTime maxDate = dateList.Max();
+
+    Dictionary<string, List<decimal>> statisticsDict = statisticsTable.GetStatisticDictByField(writeColumnName, subConstriant);
+
+    using StreamWriter file = new(
+        path: $"./{writePrefix}_{minDate:yyyy-MM-dd}_{maxDate:yyyy-MM-dd}_{writeColumnName}.csv",
+        append: false,
+        encoding: System.Text.Encoding.UTF8);
+    // write the first column of format: Timestamp, 2021/2/4 20:44:12, 2021/2/4 20:46:45
+    file.Write("Timestamp");
+    foreach (DateTime dateTime in statisticsTable.GetDateTimeList())
+    {
+        file.Write($",{dateTime:yyyy/MM/dd HH:mm:ss}");
+    }
+    file.Write('\n');
+
+    // then write the data by the order of the latest value
+    foreach (KeyValuePair<string, List<decimal>> writtenValue in statisticsDict.OrderByDescending(p => p.Value.Last()))
+    {
+        string line = writtenValue.Key;
+        List<decimal> copiedList = writtenValue.Value;
+
+        foreach (decimal value in copiedList)
+        {
+            string stringToWrite = (value > 0m) ? value.ToString() : "";
+            line += ',' + stringToWrite;
+        }
+        file.Write(line);
+        file.Write('\n');
+    }
+    file.Close();
+}
---
+++ GenerateGraph/StatisticsTable.cs
@@ -0,0 +1,228 @@
+﻿using Common.Types;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateGraph;
+// column: Channel name
+// row: DateTime
+class StatisticsTable : DataTable
+{
+    private readonly TrackList _TrackList;
+    private readonly List<DateTime> _RowDateTime;
+
+    public StatisticsTable(string trackListFilePath, int requiredLevel, bool byGroup)
+    {
+        _TrackList = new TrackList(trackListFilePath, requiredLevel, throwOnValidationFail: true);
+        _RowDateTime = new List<DateTime>();
+
+        if (byGroup)
+        {
+            foreach (string dispalyName in _TrackList.GetGroupNameList())
+            {
+                this.Columns.Add(dispalyName, typeof(VTuberStatistics));
+            }
+        }
+        else
+        {
+            foreach (string dispalyName in _TrackList.GetDisplayNameList())
+            {
+                this.Columns.Add(dispalyName, typeof(VTuberStatistics));
+            }
+        }
+
+    }
+
+    public void AddRow(DateTime dateTime, Dictionary<string, VTuberStatistics> statisticsDict)
+    {
+        _RowDateTime.Add(dateTime);
+
+        DataRow dataRow = this.NewRow();
+        foreach (KeyValuePair<string, VTuberStatistics> channelStat in statisticsDict)
+        {
+            string channelName = channelStat.Key;
+            VTuberStatistics stat = channelStat.Value;
+
+            dataRow[channelName] = stat;
+        }
+
+        this.Rows.Add(dataRow);
+    }
+
+    public void FillEmptyValueByInterpolation()
+    {
+        int rowCount = this.Rows.Count;
+        foreach (DataColumn column in this.Columns)
+        {
+            int index = 0;
+            foreach (DataRow row in this.Rows)
+            {
+                // only iterate through [1,rowCount-2]
+                if (index == 0 || index == rowCount - 1)
+                {
+                    goto END_LABEL;
+                }
+
+                object currentStatObject = row[column.ColumnName];
+                if (currentStatObject.GetType() != typeof(VTuberStatistics))
+                {
+                    Tuple<int, int, VTuberStatistics, VTuberStatistics>? nearestIndex = GetNearestStatistics(column, index);
+                    if (nearestIndex != null)
+                    {
+                        int nearestPre = nearestIndex.Item1;
+                        int nearestPost = nearestIndex.Item2;
+                        VTuberStatistics nearestPreStat = nearestIndex.Item3;
+                        VTuberStatistics nearestPostStat = nearestIndex.Item4;
+
+                        VTuberStatistics interpolationStat = GetStatisticsByInterpolation(targetDateTime: _RowDateTime[index],
+                            preDateTime: _RowDateTime[nearestPre],
+                            postDateTime: _RowDateTime[nearestPost],
+                            nearestPreStat,
+                            nearestPostStat);
+
+                        row[column.ColumnName] = interpolationStat;
+                    }
+                }
+
+            END_LABEL:
+                index++;
+            }
+        }
+    }
+
+    public List<DateTime> GetDateTimeList()
+    {
+        return new(_RowDateTime);
+    }
+
+    private static object? GetPropValue(object? obj, string propertyName)
+    {
+        foreach (string part in propertyName.Split('.'))
+        {
+            if (obj == null)
+            { return null; }
+
+            Type type = obj.GetType();
+            System.Reflection.PropertyInfo? info = type.GetProperty(part);
+            if (info == null)
+            { return null; }
+
+            obj = info.GetValue(obj, null);
+        }
+        return obj;
+    }
+
+    public Dictionary<string, List<decimal>> GetStatisticDictByField(string fieldName, decimal? youTubeSubscriberCountConstriant)
+    {
+        // initialize capacity, not size
+        Dictionary<string, List<decimal>> rList = new(_RowDateTime.Count);
+
+        if (!youTubeSubscriberCountConstriant.HasValue)
+        {
+            foreach (DataColumn column in this.Columns)
+            {
+                rList.Add(column.ColumnName, new List<decimal>());
+            }
+        }
+        else
+        {
+            DataRow lastRow = this.Rows[this.Rows.Count - 1];
+
+            int index = 0;
+            foreach (DataColumn column in this.Columns)
+            {
+                object statObj = lastRow[index];
+
+                if (statObj.GetType() == typeof(VTuberStatistics))
+                {
+                    decimal value = Convert.ToDecimal(GetPropValue(statObj, "YouTube.SubscriberCount"));
+
+                    if (value >= youTubeSubscriberCountConstriant)
+                        rList.Add(column.ColumnName, new List<decimal>());
+                }
+
+                index++;
+            }
+        }
+
+        foreach (DataRow row in this.Rows)
+        {
+            foreach (DataColumn column in this.Columns)
+            {
+                object statObj = row[column.ColumnName];
+
+                if (rList.ContainsKey(column.ColumnName))
+                {
+                    if (statObj.GetType() == typeof(VTuberStatistics))
+                    {
+                        decimal value = Convert.ToDecimal(GetPropValue(statObj, fieldName));
+                        rList[column.ColumnName].Add(value);
+                    }
+                    else
+                    {
+                        rList[column.ColumnName].Add(0m);
+                    }
+                }
+            }
+        }
+
+        return rList;
+    }
+
+    private Tuple<int, int, VTuberStatistics, VTuberStatistics>? GetNearestStatistics(DataColumn targetColumn, int targetIndex)
+    {
+        string targetColumnName = targetColumn.ColumnName;
+
+        int nearestPre = int.MinValue;
+        int nearestPost = int.MaxValue;
+
+        VTuberStatistics? nearestPreStat = null;
+        VTuberStatistics? nearestPostStat = null;
+
+        int index = 0;
+        foreach (DataRow row in this.Rows)
+        {
+            if (index == targetIndex)
+            {
+                goto END_LABEL;
+            }
+
+            object currentStatObject = row[targetColumnName];
+            if (currentStatObject.GetType() == typeof(VTuberStatistics))
+            {
+                if (index > nearestPre && index < targetIndex)
+                {
+                    nearestPre = index;
+                    nearestPreStat = (VTuberStatistics)currentStatObject;
+                }
+
+                if (index < nearestPost && index > targetIndex)
+                {
+                    nearestPost = index;
+                    nearestPostStat = (VTuberStatistics)currentStatObject;
+                }
+            }
+
+        END_LABEL:
+            index++;
+        }
+
+        if (nearestPreStat != null && nearestPostStat != null)
+            return new(nearestPre, nearestPost, nearestPreStat, nearestPostStat);
+        else
+            return null;
+    }
+
+    private static VTuberStatistics GetStatisticsByInterpolation(DateTime targetDateTime, DateTime preDateTime, DateTime postDateTime, VTuberStatistics preStat, VTuberStatistics postStat)
+    {
+        TimeSpan preInterval = targetDateTime - preDateTime;
+        TimeSpan postInterval = postDateTime - targetDateTime;
+
+        decimal preRatio = (decimal)(postInterval.TotalMilliseconds / (preInterval + postInterval).TotalMilliseconds);
+
+        return VTuberStatistics.GenerateStatisticsByInterpolation(preRatio, preStat, postStat);
+    }
+}
