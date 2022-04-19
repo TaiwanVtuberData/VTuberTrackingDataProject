e2980185e9b1c902231763b96411b27994d48790
nh60211as <nh60211@gmail.com>
2022-04-19 17:34:16 +0800

    Add missing class file

---
+++ GenerateJsonFile/Types/VTuberFullData.cs
@@ -0,0 +1,14 @@
+﻿namespace GenerateJsonFile.Types;
+
+internal readonly record struct VTuberFullData(
+    string id,
+    Activity activity,
+    string name,
+    string? imgUrl,
+    YouTubeData? YouTube,
+    TwitchData? Twitch,
+    VideoInfo? popularVideo,
+    string? group,
+    string? nationality,
+    string? debutDate,
+    string? graduateDate);
