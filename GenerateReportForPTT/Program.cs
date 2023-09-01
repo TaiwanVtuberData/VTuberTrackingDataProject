808d7e848ac93fc21a79213e425f0df7e9a3cd4d
nh60211as <nh60211@gmail.com>
2023-09-02 07:42:00 +0800

    Fix issue where ParseDateTimeList() format does not align with other projects

--- GenerateReportForPTT/Program.cs
+++ GenerateReportForPTT/Program.cs
@@ -15,7 +15,7 @@ ChannelTable channelTable = new(valueHeader: columnHeader, sortByIncreasePercent
 StreamReader file = new(command[0]);
 string? line = file.ReadLine();
 if (line == null) {
-  return;
+    return;
 }
 
 List<DateTime> dateTimeList = ParseDateTimeList(line);
@@ -24,27 +24,27 @@ int lastWeekIndex = FindClosestIndex(dateTimeList, dateTimeList.Last(), new Time
 int lastMonthIndex = FindClosestIndex(dateTimeList, dateTimeList.Last(), new TimeSpan(days: 28, hours: 0, minutes: 0, seconds: 0));
 
 while ((line = file.ReadLine()) != null) {
-  List<string> stringBlock = line.Split(',').ToList();
-  string channelName = stringBlock[0];
+    List<string> stringBlock = line.Split(',').ToList();
+    string channelName = stringBlock[0];
 
-  stringBlock = stringBlock.Skip(1).ToList();
-  if (stringBlock.Count != dateTimeList.Count)
-    throw new Exception("Data count doesn't match.");
+    stringBlock = stringBlock.Skip(1).ToList();
+    if (stringBlock.Count != dateTimeList.Count)
+        throw new Exception("Data count doesn't match.");
 
-  int thisLastWeekIndex = lastWeekIndex;
-  bool isLesserThanLastWeek = false;
-  if (accountForLessThanAWeek) {
-    for (thisLastWeekIndex = lastWeekIndex; thisLastWeekIndex < stringBlock.Count; thisLastWeekIndex++) {
-      if (stringBlock[thisLastWeekIndex] != "")
-        break;
+    int thisLastWeekIndex = lastWeekIndex;
+    bool isLesserThanLastWeek = false;
+    if (accountForLessThanAWeek) {
+        for (thisLastWeekIndex = lastWeekIndex; thisLastWeekIndex < stringBlock.Count; thisLastWeekIndex++) {
+            if (stringBlock[thisLastWeekIndex] != "")
+                break;
+        }
+        thisLastWeekIndex = Math.Min(thisLastWeekIndex, latestIndex);
+
+        if (stringBlock[lastWeekIndex] == "")
+            isLesserThanLastWeek = true;
     }
-    thisLastWeekIndex = Math.Min(thisLastWeekIndex, latestIndex);
 
-    if (stringBlock[lastWeekIndex] == "")
-      isLesserThanLastWeek = true;
-  }
-
-  channelTable.AddChannel(channelName, stringBlock[latestIndex], stringBlock[thisLastWeekIndex], stringBlock[lastMonthIndex], isLesserThanLastWeek);
+    channelTable.AddChannel(channelName, stringBlock[latestIndex], stringBlock[thisLastWeekIndex], stringBlock[lastMonthIndex], isLesserThanLastWeek);
 }
 
 file.Close();
@@ -54,41 +54,41 @@ writer.Write(channelTable.ToString(maxColumnLength));
 writer.Close();
 
 static string GetStringFromFile(string filePath) {
-  string? line;
+    string? line;
 
-  // Read the file and display it line by line.  
-  using StreamReader file = new(filePath);
-  if ((line = file.ReadLine()) != null) {
-    file.Close();
-    return line;
-  }
+    // Read the file and display it line by line.  
+    using StreamReader file = new(filePath);
+    if ((line = file.ReadLine()) != null) {
+        file.Close();
+        return line;
+    }
 
-  throw new Exception("Could not retrieve file path.");
+    throw new Exception("Could not retrieve file path.");
 }
 
 static List<DateTime> ParseDateTimeList(string data) {
-  string[] stringBlock = data.Split(',');
+    string[] stringBlock = data.Split(',');
 
-  List<DateTime> ans = new();
-  foreach (string str in stringBlock) {
-    if (DateTime.TryParseExact(str, "yyyy/MM/dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDateTime)) {
-      ans.Add(parsedDateTime);
+    List<DateTime> ans = new();
+    foreach (string str in stringBlock) {
+        if (DateTime.TryParseExact(str, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDateTime)) {
+            ans.Add(parsedDateTime);
+        }
     }
-  }
 
-  return ans;
+    return ans;
 }
 
 static int FindClosestIndex(List<DateTime> dateTimes, DateTime target, TimeSpan timeSpan) {
-  TimeSpan minTimeSpan = TimeSpan.MaxValue;
-  int minIndex = -1;
-  for (int i = 0; i < dateTimes.Count; i++) {
-    TimeSpan timeDifference = (timeSpan - (target - dateTimes[i]).Duration()).Duration();
-    if (minTimeSpan > timeDifference) {
-      minTimeSpan = timeDifference;
-      minIndex = i;
+    TimeSpan minTimeSpan = TimeSpan.MaxValue;
+    int minIndex = -1;
+    for (int i = 0; i < dateTimes.Count; i++) {
+        TimeSpan timeDifference = (timeSpan - (target - dateTimes[i]).Duration()).Duration();
+        if (minTimeSpan > timeDifference) {
+            minTimeSpan = timeDifference;
+            minIndex = i;
+        }
     }
-  }
 
-  return minIndex;
+    return minIndex;
 }
