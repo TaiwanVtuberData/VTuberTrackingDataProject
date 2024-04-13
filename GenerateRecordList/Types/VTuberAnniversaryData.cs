43fc497e31af3a4b8e871fd026268f18d22fbf61
nh60211as <45863601+nh60211as@users.noreply.github.com>
2024-04-13 18:54:11 +0800

    Implement Anniversary VTuber API (#17)
    * https://github.com/TaiwanVtuberData/TaiwanVTuberData.github.io/issues/118 Add APIs for VTubers anniversary.
    
    * https://github.com/TaiwanVtuberData/TaiwanVTuberData.github.io/issues/118 add field VTuberAnniversaryData::debutDateOfTheYear

---
+++ GenerateRecordList/Types/VTuberAnniversaryData.cs
@@ -0,0 +1,22 @@
+﻿using Common.Types.Basic;
+using Common.Utils;
+using System.Text.Json.Serialization;
+
+namespace GenerateRecordList.Types;
+
+public record VTuberAnniversaryData(
+    [property: JsonConverter(typeof(VTuberIdJsonConverter))] VTuberId id,
+    Activity activity,
+    string name,
+    string? imgUrl,
+    YouTubeData? YouTube,
+    TwitchData? Twitch,
+    VideoInfo? popularVideo,
+    string? group,
+    string? nationality,
+    string debutDate,
+    string debutDateOfTheYear,
+    uint anniversaryYearCount);
+
+public record VTuberAnniversaryDataResponse(
+    List<VTuberAnniversaryData> VTubers);
